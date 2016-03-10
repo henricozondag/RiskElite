3b161fb3d84da47d3b28314e91be49611fee3409
marcelsteur <mjp.steur@gmail.com>
2016-03-10 23:43:45 +0100

    Splash door naar menu

--- app/src/main/java/mamahetogames/riskelite/SplashScreen.java
+++ app/src/main/java/mamahetogames/riskelite/SplashScreen.java
@@ -1,7 +1,9 @@
 package mamahetogames.riskelite;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        // Wacht 2500ms en ga door naar menu
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                Intent i;
+                i = new Intent(SplashScreen.this, Menu.class);
+                startActivity(i);
+            }
+        }, 2500);
+
     }
+
 }
