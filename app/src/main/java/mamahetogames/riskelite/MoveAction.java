691d3ef1068bd63487777d48808a6cd200aa0a7c
Henrico <henricozondag@gmail.com>
2016-03-13 22:17:42 +0100

    Ik kan al 1 dobbelsteen gooien. :)

--- app/src/main/java/mamahetogames/riskelite/MoveAction.java
+++ app/src/main/java/mamahetogames/riskelite/MoveAction.java
@@ -1,12 +1,18 @@
 package mamahetogames.riskelite;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+        import android.content.Intent;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.ImageView;
+
+        import java.util.Random;
 
 public class MoveAction extends AppCompatActivity implements View.OnClickListener{
+
+    int myResult;
+    ImageView imageView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +21,39 @@ public class MoveAction extends AppCompatActivity implements View.OnClickListene
 
         Button buttonMovePhase2 = (Button) findViewById(R.id.buttonMovePhase2);
         buttonMovePhase2.setOnClickListener(this);
+        Button buttonGooiAttack = (Button) findViewById(R.id.buttonGooiAttack);
+        buttonGooiAttack.setOnClickListener(this);
+        imageView2 = (ImageView)findViewById(R.id.imageView2);;
+    }
+
+    public void rollDice() {
+        myResult = new Random(System.currentTimeMillis()).nextInt(6)+1;
+        switch(myResult){
+            case 1:
+                imageView2.setImageResource(R.mipmap.dice_1);
+                break;
+
+            case 2:
+                imageView2.setImageResource(R.mipmap.dice_2);
+                break;
+
+            case 3:
+                imageView2.setImageResource(R.mipmap.dice_3);
+                break;
+
+            case 4:
+                imageView2.setImageResource(R.mipmap.dice_4);
+                break;
+
+            case 5:
+                imageView2.setImageResource(R.mipmap.dice_5);
+                break;
+
+            case 6:
+                imageView2.setImageResource(R.mipmap.dice_6);
+                break;
+
+        }
     }
 
     @Override
@@ -24,6 +63,9 @@ public class MoveAction extends AppCompatActivity implements View.OnClickListene
             case R.id.buttonMovePhase2:
                 i = new Intent(this, MovePhase2.class);
                 startActivity(i);
+                break;
+            case R.id.buttonGooiAttack:
+                rollDice();
                 break;
         }
     }
